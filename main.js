7071df9f3b7c4610662005ed4ee352f1ed67650e
samuelraj-dev <samuelraj.dev@outlook.com>
2023-08-14 19:40:43 +0530

    Third commit - Updated main.js - Port 5000

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { connectDB } from './db.js'
 import workoutsRoutes from './routes/workout.js'
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.use((req, res, next) => {
     console.log(req.method, req.url)
