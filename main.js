4246129552e325c77f2e313107178ef0d47becdc
samuelraj-dev <samuelraj.dev@outlook.com>
2023-08-14 20:28:34 +0530

    Fifth commit - added cors

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'dotenv/config'
+import cors from 'cors'
 
 import { connectDB } from './db.js'
 
@@ -7,6 +8,8 @@ import workoutsRoutes from './routes/workout.js'
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log(req.method, req.url)
